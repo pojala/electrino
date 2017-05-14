b0621763203a826ae05dab571fd5d5c4a6b3e75b
Thomas Haakon Townsend <thomas.haakon@gmail.com>
2017-05-14 23:39:44 +0200

    Basic jsvm working

--- Electrino/win10/Electrino/App.xaml.cs
+++ Electrino/win10/Electrino/App.xaml.cs
@@ -22,6 +22,8 @@ namespace Electrino
     /// </summary>
     sealed partial class App : Application
     {
+        private JavaScriptApp jsApp = new JavaScriptApp();
+
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
@@ -30,6 +32,8 @@ namespace Electrino
         {
             this.InitializeComponent();
             this.Suspending += OnSuspending;
+            System.Diagnostics.Debug.WriteLine(jsApp.init());
+            System.Diagnostics.Debug.WriteLine(jsApp.runScript("require.toString()"));
         }
 
         /// <summary>
--- Electrino/win10/Electrino/JavaScriptApp.cs
+++ Electrino/win10/Electrino/JavaScriptApp.cs
@@ -3,11 +3,138 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
+using System.Runtime.InteropServices;
+using ChakraHost.Hosting;
 
 namespace Electrino
 {
     class JavaScriptApp
     {
-        ChakraHost.ChakraHost host = new ChakraHost.ChakraHost();
+        private JavaScriptSourceContext currentSourceContext = JavaScriptSourceContext.FromIntPtr(IntPtr.Zero);
+        private JavaScriptRuntime runtime;
+        private Dictionary<string, string> jsModules = new Dictionary<string, string>();
+        private JavaScriptValue require;
+        private JavaScriptValue jsAppGlobalObject;
+        private static Queue taskQueue = new Queue();
+        private static readonly JavaScriptPromiseContinuationCallback promiseContinuationDelegate = promiseContinuationCallback;
+
+        private static void promiseContinuationCallback(JavaScriptValue task, IntPtr callbackState)
+        {
+            taskQueue.Enqueue(task);
+            task.AddRef();
+        }
+
+        public string init()
+        {
+            JavaScriptContext context;
+
+            if (Native.JsCreateRuntime(JavaScriptRuntimeAttributes.None, null, out runtime) != JavaScriptErrorCode.NoError)
+                return "failed to create runtime.";
+
+            if (Native.JsCreateContext(runtime, out context) != JavaScriptErrorCode.NoError)
+                return "failed to create execution context.";
+
+            if (Native.JsSetCurrentContext(context) != JavaScriptErrorCode.NoError)
+                return "failed to set current context.";
+
+
+            if (Native.JsSetPromiseContinuationCallback(promiseContinuationDelegate, IntPtr.Zero) != JavaScriptErrorCode.NoError)
+                return "failed to setup callback for ES6 Promise";
+
+            if (Native.JsProjectWinRTNamespace("Windows") != JavaScriptErrorCode.NoError)
+                return "failed to project windows namespace.";
+
+            if (Native.JsStartDebugging() != JavaScriptErrorCode.NoError)
+                return "failed to start debugging.";
+
+
+
+            if (Native.JsCreateObject(out require) != JavaScriptErrorCode.NoError)
+                return "failed to create require";
+            if (Native.JsGetGlobalObject(out jsAppGlobalObject) != JavaScriptErrorCode.NoError)
+                return "failed to get global object";
+
+            if (Native.JsSetProperty(jsAppGlobalObject, JavaScriptPropertyId.FromString("require"), require, false) != JavaScriptErrorCode.NoError)
+                return "failed to define require on global";
+
+
+            JavaScriptValue requireToString;
+            JavaScriptNativeFunction echoDelegate = Echo;
+            if (Native.JsCreateFunction(echoDelegate, IntPtr.Zero, out requireToString) != JavaScriptErrorCode.NoError)
+                return "failed to create require toString function";
+            if (Native.JsSetProperty(require, JavaScriptPropertyId.FromString("toString"), requireToString, false) != JavaScriptErrorCode.NoError)
+                return "failed to define tostring on require";
+
+
+
+            return "NoError";
+        }
+
+        private static JavaScriptValue Echo(JavaScriptValue callee, bool isConstructCall, JavaScriptValue[] arguments, ushort argumentCount, IntPtr callbackData)
+        {
+            System.Diagnostics.Debug.WriteLine("require.tostring called");
+            return JavaScriptValue.Invalid;
+        }
+
+        public string runScript(string script)
+        {
+            IntPtr returnValue;
+
+            try
+            {
+                JavaScriptValue result;
+
+                if (Native.JsRunScript(script, currentSourceContext++, "", out result) != JavaScriptErrorCode.NoError)
+                {
+                    // Get error message and clear exception
+                    JavaScriptValue exception;
+                    if (Native.JsGetAndClearException(out exception) != JavaScriptErrorCode.NoError)
+                        return "failed to get and clear exception";
+
+                    JavaScriptPropertyId messageName;
+                    if (Native.JsGetPropertyIdFromName("message",
+                        out messageName) != JavaScriptErrorCode.NoError)
+                        return "failed to get error message id";
+
+                    JavaScriptValue messageValue;
+                    if (Native.JsGetProperty(exception, messageName, out messageValue)
+                        != JavaScriptErrorCode.NoError)
+                        return "failed to get error message";
+
+                    IntPtr message;
+                    UIntPtr length;
+                    if (Native.JsStringToPointer(messageValue, out message, out length) != JavaScriptErrorCode.NoError)
+                        return "failed to convert error message";
+
+                    return Marshal.PtrToStringUni(message);
+                }
+                
+                // Execute promise tasks stored in taskQueue 
+                while (taskQueue.Count != 0)
+                {                
+                    JavaScriptValue task = (JavaScriptValue) taskQueue.Dequeue();
+                    JavaScriptValue promiseResult;
+                    JavaScriptValue[] args = new JavaScriptValue[1] {jsAppGlobalObject};
+                    Native.JsCallFunction(task, args, 1, out promiseResult);
+                    task.Release();
+                }
+
+                // Convert the return value.
+                JavaScriptValue stringResult;
+                UIntPtr stringLength;
+                if (Native.JsConvertValueToString(result, out stringResult) != JavaScriptErrorCode.NoError)
+                    return "failed to convert value to string.";
+                if (Native.JsStringToPointer(stringResult, out returnValue, out stringLength) != JavaScriptErrorCode.NoError)
+                    return "failed to convert return value.";
+            }
+            catch (Exception e)
+            {
+                return "chakrahost: fatal error: internal error: " + e.Message;
+            }
+
+            return Marshal.PtrToStringUni(returnValue);
+        }
+
     }
 }
