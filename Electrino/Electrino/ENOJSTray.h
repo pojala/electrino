48904aa95812c547c2de2b7b38f29c532f251628
Pauli Ojala <pauli@lacquer.fi>
2017-05-22 22:09:10 +0300

    Fixed missing MIT license notice in some files

--- Electrino/Electrino/ENOJSTray.h
+++ Electrino/Electrino/ENOJSTray.h
@@ -3,7 +3,9 @@
 //  Electrino
 //
 //  Created by Pauli Ojala on 17/05/2017.
-//  Copyright © 2017 Lacquer. All rights reserved.
+//  Copyright © 2017 Pauli Olavi Ojala.
+//
+//  This software may be modified and distributed under the terms of the MIT license.  See the LICENSE file for details.
 //
 
 #import <Foundation/Foundation.h>
